20181f1bd647cecebcfc1e5b523d4666a2b0f825
Thomas O'Connor <thomas.Ocono@gmail.com>
2026-02-07 20:59:45 -0500

    connected to MongoDB, and added env

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,43 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+const Schema = mongoose.Schema;
 
+dotenv.config();
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("goodbye");
+const connectToDB = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URI);
+    console.log("Connected to DB");
+  } catch (error) {
+    console.error(error);
+    console.log("Error connecting to DB");
+  }
+};
+
+connectToDB();
+
+const testSchema = new Schema({
+  text: {
+    type: "String",
+    required: true,
+  },
 });
+const testUser = mongoose.model("User", testSchema);
+
+const testData = async () => {
+  const newTest = new testUser({
+    text: "Hello World",
+  });
+  try {
+    newTest.save();
+    console.log("Data saved");
+  } catch (error) {
+    console.error(error);
+    console.log("Error saving");
+  }
+};
 
 app.listen(5001, () => {
   console.log("Server started on port 5001");
