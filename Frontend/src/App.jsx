f838267c3647a5226441ecd982dfea84873170e9
Thomas O'Connor <thomas.Ocono@gmail.com>
2026-02-07 20:25:07 -0500

    Start of project

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import axios from "axios";
+import Feed from "./Feed.jsx";
+import Input from "./InputPosts.jsx";
+
+function App() {
+  return (
+    <>
+      <Input />
+      <Feed />
+    </>
+  );
+}
+
+export default App;
---
+++ Frontend/src/InputPosts.jsx
@@ -0,0 +1,10 @@
+function Input() {
+  return (
+    <>
+      <input type="text" placeholder="What's Happening?"></input>
+      <button>Post</button>
+    </>
+  );
+}
+
+export default Input;
