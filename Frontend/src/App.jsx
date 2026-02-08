676301f024686131d058872e7cab5d8cf3794b0e
Thomas O'Connor <thomas.Ocono@gmail.com>
2026-02-07 22:57:35 -0500

    messing around with react-router

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css";
+import { Route, Routes } from "react-router";
 import axios from "axios";
-import Feed from "./Feed.jsx";
-import Input from "./InputPosts.jsx";
+import UserStuff from "./UserStuff.jsx";
 
 function App() {
   return (
     <>
-      <Input />
-      <Feed />
+      <Routes>
+        <Route path="/login" element={<UserStuff />} />
+      </Routes>
     </>
   );
 }
---
+++ Frontend/src/UserStuff.jsx
@@ -0,0 +1,24 @@
+let isLoggedIn = false;
+
+function UserStuff() {
+  if (!isLoggedIn) {
+    return (
+      <>
+        <p>Username</p>
+        <input type="text" />
+        <p>Password</p>
+        <input type="text" />
+        <button>Login</button>
+        <button>Register</button>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <h1>Hello there Username!</h1>
+      </>
+    );
+  }
+}
+
+export default UserStuff;
