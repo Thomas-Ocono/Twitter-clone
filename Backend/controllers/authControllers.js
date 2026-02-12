1ea8ca8f7f0466a1919104e3d51f93524ea716be
Thomas O'Connor <thomas.Ocono@gmail.com>
2026-02-11 22:08:26 -0500

    Auth work, this is harder than I thought

--- Backend/controllers/authControllers.js
+++ Backend/controllers/authControllers.js
@@ -1,6 +1,7 @@
 import express from "express";
 import User from "../models/Users.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const registerUser = async (req, res) => {
   try {
@@ -24,14 +25,19 @@ export const registerUser = async (req, res) => {
 export const loginUser = async (req, res) => {
   try {
     const user = await User.findOne({ username: req.body.username });
-
     if (!user) {
       res.send({ message: "User not found" });
       return;
     }
 
     if (await bcrypt.compare(req.body.password, user.password)) {
-      res.send({ message: "Successful Login" });
+      const accessToken = jwt.sign(user.username, process.env.JWT_SECRET);
+      res.cookie("token", accessToken, {
+        httpOnly: true,
+        sameSite: "lax",
+        secure: false,
+      });
+      res.send({ message: "Success!" });
     } else {
       res.send({ message: "Not authorized" });
     }
@@ -39,5 +45,22 @@ export const loginUser = async (req, res) => {
     console.log(error);
     console.log("error logging in user");
     res.send({ message: "Error logging in user" });
+  }
+};
+
+export const checkToken = (req, res) => {
+  const token = req.cookies.token;
+  console.log(token);
+  if (!token) {
+    res.status(401).json({ message: "No Cookie" });
+    return;
+  }
+  console.log("Trying");
+  try {
+    const checkCookie = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(checkCookie);
+    res.json({ message: "Cookie Verified :D" });
+  } catch (error) {
+    return res.status(401).json({ message: "invalid Cookie" });
   }
 };
--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -2,12 +2,18 @@ import express from "express";
 import Users from "../models/Users.js";
 import jwt from "jsonwebtoken";
 
-import { registerUser, loginUser } from "../controllers/authControllers.js";
+import {
+  registerUser,
+  loginUser,
+  checkToken,
+} from "../controllers/authControllers.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 
 router.get("/login", loginUser);
+
+router.get("/checkToken", checkToken);
 
 export default router;
