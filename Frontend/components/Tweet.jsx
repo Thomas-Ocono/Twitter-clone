beebbfb794f5ac4bcbad59c19078d950eda6b2e4
Thomas O'Connor <thomas.Ocono@gmail.com>
2026-02-13 15:52:52 -0500

    Restarting project

--- Frontend/components/Tweet.jsx
+++
@@ -1,9 +0,0 @@
-function Tweet({ username, content }) {
-  return (
-    <div class="Tweet">
-      <h2>{username} </h2>
-      <p>{content}</p>
-    </div>
-  );
-}
-export default Tweet;
--- Frontend/src/Feed.jsx
+++
@@ -1,5 +0,0 @@
-function Feed() {
-  return <></>;
-}
-
-export default Feed;
